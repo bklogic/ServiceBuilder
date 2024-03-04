41a0f78b120718bddeb5a7ec19e04965729ce43a
ken <ken@backlogic.net>
2024-03-03 18:45:57 -0800

    remove outfolder from git

--- out/core/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dataSourceFolderName = exports.namespace = void 0;
-exports.namespace = 'servicebuilder';
-exports.dataSourceFolderName = '.datasource';
-//# sourceMappingURL=constants.js.map
